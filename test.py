36bed13b3f5a0f079d4ccc3ab659421238a6f0a7
Shine <b02901069@ntu.edu.tw>
2016-11-22 21:47:01 +0800

    these two codes are the UART transmission code in C ad python respectively.

---
+++ test.py
@@ -0,0 +1,15 @@
+import serial
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=115200,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.SEVENBITS
+)
+
+while True:
+	read_data = ser.read(20)
+	data = read_data.encode('hex')
+	print(data)
