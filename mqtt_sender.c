491265b99038ac888f445eec5a7b01089d11e439
lintzuhsiang <b02901069@ntu.edu.tw>
2017-02-10 12:46:52 +0800

    mqtt_sender

--- mqtt_sender.c
+++ mqtt_sender.c
@@ -79,18 +79,6 @@ int main(int argc,char **argv){
 	fflush(stdout);
 	FILE *src = stdin;
 
-	//allocate the memory
-	// if(argc > MQTT_INPUT_INDEX_SERVER_COUNT + 1)
-	// 	numServer = atoi(argv[MQTT_INPUT_INDEX_SERVER_PORT]);
-	// printf("Total number of server:%d\n",numServer);
-	// fflush(stdout);
-
-
-	//host = calloc(numServer,sizeof(char));
-	//port = calloc(numServer,sizeof(int));
-
-	//host[0] = argv[MQTT_INPUT_INDEX_SERVER_ADDR];
-	//port[0] = atoi(argv[INDEX_INDEX]);
 
 	mosquitto_lib_init();    //initial the mosquitto function
   mosq = mosquitto_new(mqttClientId, 1, NULL);  //create a new mosquitto client
@@ -103,7 +91,12 @@ int main(int argc,char **argv){
   mosquitto_username_pw_set(mosq, mqttUsername, mqttPasswd);   //configure a username and passwd
   mosquitto_connect_callback_set(mosq,callback);  //set the connect callback
   mosquitto_connect_async(mosq, host, port, 60);  //connect to a mqtt broker
-  mosquitto_loop_start(mosq);  //start the loop
+	if(mosquitto_connect_async(mosq, host, port, 60) == MOSQ_ERR_SUCCESS	)
+	mosquitto_loop_start(mosq);  //start the loop
+
+	mosquitto_connect(mosq, host, port, 60);  //connect to a mqtt broker
+  mosquitto_loop(mosq);  //start the loop
+
 	mosquitto_publish(mosq,NULL,topic,strlen(message),message,qos,0);
 	if(mosquitto_publish(mosq,NULL,topic,strlen(message),message,qos,0) == MOSQ_ERR_SUCCESS){
 		printf("success on publish.\n");
@@ -112,45 +105,13 @@ int main(int argc,char **argv){
 		printf("fail on publish.\n");
 		fflush(stdout);
 	}
-    // //for multi servers
-    // for(serverIndex = 1; serverIndex < numServer; serverIndex++) {
-    //     mosq[serverIndex] = mosquitto_new(mqttClientId, 1, NULL);
-    //     host[serverIndex] = argv[4 + 2 * (serverIndex - 1) + 1];
-    //     port[serverIndex] = atoi(argv[4 + 2 * (serverIndex - 1) + 2]);
-    //     printf("[%d] host: %s, port: %d\n", serverIndex + 1, host[serverIndex], port[serverIndex]);
-    //     fflush(stdout);
 
-    //     mosquitto_username_pw_set(mosq[serverIndex], mqttUsername, NULL);
-    //     mosquitto_connect_async(mosq[serverIndex], host[serverIndex], port[serverIndex], 10);
-    //     mosquitto_loop_start(mosq[serverIndex]);
 	for(int k=0;k<5;k++){
 			char *top = "aaa";
 			char *mes = "hello world.\n";
 			mosquitto_publish(mosq,NULL,top,strlen(mes),mes,qos,0);
 	}
-/*
-  do {
-        //if (!fgets(buf, BUF_SZ, src)) //get the data from the file src, max lens is BUF_SZ
-        //   continue;
 
-        //char* str = trim(buf);
-        //if (!strlen(str)) continue;
-
-        //printf( "%s\n", str);
-        //fflush(stdout);
-
-      mosquitto_publish(mosq[0],NULL,topic,strlen(message),message,qos,0);
-        //sendMessageToTopic(mosq[serverIndex], topic, str);
-      if(mosquitto_publish(mosq[0],NULL,topic,strlen(message),message,qos,0) != MOSQ_ERR_SUCCESS)
-        mosquitto_reconnect_async(mosq[0]);
-        // for(serverIndex = 0; serverIndex < numServer; serverIndex++) {
-        //     if(sendMessageToTopic(mosq[serverIndex], topic, str) != MOSQ_ERR_SUCCESS)
-        //         mosquitto_reconnect_async(mosq[serverIndex]);
-        //}
-    } while(k<5);
-		k++;*/
-
-	    //}  while (!feof(src));
   fclose(src);
   return 0;
 
