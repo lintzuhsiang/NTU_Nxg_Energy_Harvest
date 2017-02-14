80891bd71d3b01e6c3ab9d984e3944e777a9527d
lintzyhsiang <b02901069@ntu.edu.tw>
2017-02-14 23:19:38 +0800

    mqtt_sender

--- mqtt_sender.c
+++ mqtt_sender.c
@@ -12,94 +12,78 @@
 
 
 #define BUF_SZ 1024
-//char *host ="broker.hivemq.com";
-//int *port = 1883;
-int qos = 2;
-//char topic[128] = "";
 
-void my_message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
+int qos = 0;
+
+
+
+void my_connect_callback(struct mosquitto *mosq, void *userdata, int result)
 {
-	if(message->payloadlen){
-		printf("%s %s\n", message->topic, message->payload);
-	}else{
-		printf("%s (null)\n", message->topic);
-	}
-	fflush(stdout);
+    int i;
+    printf("my_connect_callback\n");
+    if(!result) {
+        /* Subscribe to broker information topics on successful connect. */
+        mosquitto_subscribe(mosq, NULL, "$SYS/#", 2);
+    } else {
+        fprintf(stderr, "Connect failed\n");
+    }
 }
 
-void onConnectCallback(struct mosquitto *mosq,void *obj,int rc) {
-    printf("30000");
-    if(!rc) {
-        printf("Success connect to server!\n");
-
-    }
-    else {
-        printf("Fail to connect to server.\n");
-    }
-    fflush(stdout);
-}
 
 void my_subscribe_callback(struct mosquitto *mosq, void *obj, int mid, int qos_count, const int *granted_qos)
 {
-	int i;
+    int i;
+    printf("my_subscribe_callback\n");
+    printf("Subscribed (mid: %d): %d", mid, granted_qos[0]);
+    for(i=1; i<qos_count; i++) {
+        printf(", %d", granted_qos[i]);
+    }
+    printf("\n");
+}
 
-	printf("Subscribed (mid: %d): %d", mid, granted_qos[0]);
-	for(i=1; i<qos_count; i++){
-		printf(", %d", granted_qos[i]);
-	}
-	printf("\n");
+void my_publish_callback(struct mosquitto *mosq, void *obj, int mid)
+{
+    int i;
+    printf("my_publish_callback\n");
+    printf("Published : %d\n", mid);
 }
 
 int main(int argc,char **argv) {
 
-    char *host = "broker.mqttdashboard.com";
-    //char* host = "140.112.41.157";
+    char* host = "140.112.41.157";
     int port = 1883;
 
-    char* mqttClientId = "lens_UeH7aySXVo87dOnbOioUFW9L3tC";
-    //char *mqttClientId = "clientId-wcd3z7o1Tv";
-    char* mqttUsername = "shine";
-    char* mqttPasswd = "shine0125951";
+    char* mqttClientId = NULL;
+    char* mqttUsername = NULL;
+    char* mqttPasswd = NULL;
 
     char *message = "Hello world.";
     char *topic = "hello";
 
-    struct mosquitto *mosq = NULL;
+    struct mosquitto *mosq;
     char buf[BUF_SZ];
 
     int numServer = 1;
     int serverIndex = 0;
-    //int k = 0 ;
-    printf("Start to connect to server.\n");
-    fflush(stdout);
-    FILE *src = stdin;
 
+    printf("Start to connect to server.\n");
+    printf( "Topic is %s\n", topic);
+    fflush(stdout);
 
     mosquitto_lib_init();    //initial the mosquitto function
     mosq = mosquitto_new(NULL, 1, NULL);  //create a new mosquitto client
-    if(mosquitto_lib_init() == MOSQ_ERR_SUCCESS	)
-    {
-        printf("[1] host: %s, port: %d\n", host, port);
-    }
-    fflush(stdout);
+
 
     mosquitto_username_pw_set(mosq, mqttUsername, mqttPasswd);   //configure a username and passwd
-    mosquitto_connect_callback_set(mosq,onConnectCallback);  //set the connect callback
-    mosquitto_message_callback_set(mosq, my_message_callback);
-    // if(mosquitto_connect_async(mosq, host, port, 60) == MOSQ_ERR_SUCCESS	){
-    //     printf("success on connecting.\n");
-    //     mosquitto_loop_start(mosq);
-    // }  //start the loop
-    if(mosquitto_connect(mosq, host, port, 60)){
-    		fprintf(stderr, "Unable to connect.\n");
-    		return 1;
-    	}
-    else if(mosquitto_connect(mosq, host, port, 60) == MOSQ_ERR_SUCCESS	){
-        printf("success on connecting.\n");
-        mosquitto_loop(mosq,-1,1);
-  }
+    mosquitto_publish_callback_set(mosq, my_publish_callback);
+    //mosquitto_connect_callback_set(mosq,my_connect_callback);  //set the connect callback
 
-    //mosquitto_publish(mosq,NULL,topic,strlen(message),message,qos,0);
+
+    if(mosquitto_connect(mosq, host, port, 60)) {
+        fprintf(stderr, "Unable to connect.\n");
+        return 1;
+    }
+
     if( mosquitto_publish(mosq,NULL,topic,strlen(message),message,qos,0) == MOSQ_ERR_SUCCESS) {
         printf("success on publish.\n");
         fflush(stdout);
@@ -108,13 +92,6 @@ int main(int argc,char **argv) {
         fflush(stdout);
     }
 
-    for(int k=0; k<5; k++) {
-        char *top = "aaa";
-        char *mes = "hello world.\n";
-        mosquitto_publish(mosq,NULL,top,strlen(mes),mes,qos,0);
-    }
-
-    fclose(src);
+    mosquitto_loop_forever(mosq, -1, 1);
     return 0;
-
 }
